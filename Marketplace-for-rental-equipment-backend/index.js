e08dbc88209cf682f5fe546c536e810acccb2606
harishgithu <112798683+harishgithu@users.noreply.github.com>
2024-07-29 19:54:55 +0530

    rental-market

---
+++ Marketplace-for-rental-equipment-backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors());
+
+require('./Server-Connection/connection')
+const Logers = require('./Models/Loger')
+
+app.post('/',async(req,res)=>{
+   let loger = new Logers(req.body);
+   let result = await loger.save();
+   res.send(result);
+})
+
+app.listen(8000);
