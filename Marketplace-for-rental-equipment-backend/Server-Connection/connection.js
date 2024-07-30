32a5122b5cef004ffa0f32330dbc26638b54595b
harishgithu <112798683+harishgithu@users.noreply.github.com>
2024-07-30 19:46:51 +0530

    rental-market

--- Marketplace-for-rental-equipment-backend/Server-Connection/connection.js
+++ Marketplace-for-rental-equipment-backend/Server-Connection/connection.js
@@ -1,2 +1,4 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://harishthangavelp:haMongOdB@aTlAs@hamongvel.6riq9qs.mongodb.net/')
+mongoose.connect('mongodb+srv://harishthangavelp:LXxXUkf2D5j68IfH@hamongvel.6riq9qs.mongodb.net/login-details')
+// mongoose.connect('mongodb://localhost:27017/login-details')
+
