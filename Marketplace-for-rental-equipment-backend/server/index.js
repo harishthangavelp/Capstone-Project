928f0b27faa91ff8954a8293ca160938414bb10c
harishgithu <112798683+harishgithu@users.noreply.github.com>
2024-08-15 10:36:07 +0530

    rental-market

--- Marketplace-for-rental-equipment-backend/server/index.js
+++ Marketplace-for-rental-equipment-backend/server/index.js
@@ -12,25 +12,31 @@ app.use(cors())
 
 mongoose.connect("mongodb+srv://harishthangavelp:LXxXUkf2D5j68IfH@hamongvel.6riq9qs.mongodb.net/user");
 
-app.post('/login',(req,res) => {
-    const {email,password} = req.body;
-    userModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            if(user.password === password){
-                res.json('Success')
-            }
-            else{
-                res.json('Invalid Credentials')
-            }
-        }
-            else{
-                res.json('No record found')
-            }
+// app.post('/login',(req,res) => {
+//     const {email,password} = req.body;
+//     userModel.findOne({email: email})
+//     .then(user => {
+//         if(user){
+//             if(user.password === password){
+//                 res.json('Success')
+//             }
+//             else{
+//                 res.json('Invalid Credentials')
+//             }
+//         }
+//             else{
+//                 res.json('No record found')
+//             }
         
-    })
-})
+//     })
+// })
 
+
+app.post('/login',(req,res) => {
+    userModel.create(req.body)
+    .then(users => req.json(users))
+    .catch(err => res.json(err))
+    })
 
 app.post('/register',(req,res) => {
 userModel.create(req.body)
@@ -39,14 +45,10 @@ userModel.create(req.body)
 })
 
 app.get("/getUsers", async (req, res) => {
-    const userData = await userModel.findOne();
+    const userData = await userModel.find();
     res.json(userData);
 });
 
-// app.get("/gotcha",async (req,res) =>{
-//     const gotData = await userModel.findOne();
-//     res.json(gotData);
-// })
 
 app.listen(3001,() => {
     console.log('server in live')
