3df48db19bc4d8c6bf3c8916bad375d6f1c4caf8
harishgithu <112798683+harishgithu@users.noreply.github.com>
2024-10-04 20:28:39 +0530

    rental-market

--- razor_pay backend/server/index.js
+++ razor_pay backend/server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const paymentRoutes = require("../server/Routes/payment");
-const accessModel = require("../Models/Access");
+const accessModel = require("../server/Models/Access");
 
 const app = express();
 
