ac95f8b3324ae8649c6e3cf6f6da3c28af50fe7b
harishgithu <112798683+harishgithu@users.noreply.github.com>
2024-10-04 19:58:43 +0530

    rental-market

---
+++ razor_pay backend/server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const paymentRoutes = require("../server/Routes/payment");
+const accessModel = require("../Models/Access");
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+// mongoose.connect("https://capstone-project-22.onrender.com/access");
+
+app.post('/pay',(req,res) => {
+    accessModel.create(req.body)
+    .then(users => req.json(users))
+    .catch(err => res.json(err))
+    })
+
+app.get("/getpaymentdetails", async (req, res) => {
+        const payData = await accessModel.find();
+        res.json(payData);
+    });
+    
+
+app.use("/api/payment/",paymentRoutes);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
