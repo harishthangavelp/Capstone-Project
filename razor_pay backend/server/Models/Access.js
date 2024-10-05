1570f3b41a8421afd7d753e6d9f3eee15f9f7ddc
harishgithu <112798683+harishgithu@users.noreply.github.com>
2024-10-05 19:56:18 +0530

    rental-market

--- razor_pay backend/server/Models/Access.js
+++ razor_pay backend/server/Models/Access.js
@@ -1,17 +1,27 @@
-const razorpay = require('razorpay');
-const accessSchema = new razorpay.Schema({
+const mongoose = require('mongoose');
+
+// Connect to your MongoDB database
+mongoose.connect('mongodb+srv://harishthangavelp:LXxXUkf2D5j68IfH@hamongvel.6riq9qs.mongodb.net/user', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Define your Mongoose schema
+const accessSchema = new mongoose.Schema({
     data: String,
     key: String,
     amount: Number,
-    currency: Number,
+    currency: String, // Should typically be a string like 'INR'
     name: String,
     description: String,
     image: String,
     order_id: String,
     orderUrl: String
-})
+});
 
-const accessModel = razorpay.model('access',accessSchema)
-module.exports = accessModel
+// Create a Mongoose model
+const accessModel = mongoose.model('Access', accessSchema);
+
+module.exports = accessModel;
 
 // module.exports = razorpay.model('access',accessSchema)
