871f0517d43f9456de7de98fc6ba721f658b9831
harishgithu <112798683+harishgithu@users.noreply.github.com>
2024-10-31 12:16:10 +0530

    rental-market

---
+++ Marketplace-for-rental-equipment-frontend/Rental-Storet/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    plugins: [
+      nodePolyfills(),
+      react()
+    ],
+    server:{
+        // port: 5173
+    }
+  })
